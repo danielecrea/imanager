eeaca2002a50f0568942ee5fa3085f6ef86e5a6d
Your Name <you@example.com>
2017-12-06 17:47:36 +0530

    Removing the HotelManager.php file

--- application/controllers/HotelManager/HotelManager.php
+++
@@ -1,8 +0,0 @@
-abstract class HotelManager()
-{
-	abstract function getStandardPrices();
-	abstract function getActualPrices();
-	abstract function getRealAvailabilities();
-	abstract function setOTAPrices();
-	abstract function setOfficialWebsitePrices();
-}
