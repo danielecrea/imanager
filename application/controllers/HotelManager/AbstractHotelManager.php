2e3ca41286a43a692f1c33fed80d9595cbfcd4af
Your Name <you@example.com>
2017-12-06 17:54:37 +0530

    adding the proper name

---
+++ application/controllers/HotelManager/AbstractHotelManager.php
@@ -0,0 +1,8 @@
+abstract class AbstractHotelManager()
+{
+	abstract function getStandardPrices();
+	abstract function getActualPrices();
+	abstract function getRealAvailabilities();
+	abstract function setOTAPrices();
+	abstract function setOfficialWebsitePrices();
+}
